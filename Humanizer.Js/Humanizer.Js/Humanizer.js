bd71b79742fa6873b50306e784998e9c2609e145
Shmueli Englard <shmueli.yosef@englard.net>
2014-06-22 12:02:05 -0400

    Added classes NumberToWordsExtension and NumberToOrdinalWordsExtension

--- Humanizer.Js/Humanizer.Js/Humanizer.js
+++ Humanizer.Js/Humanizer.Js/Humanizer.js
@@ -36,6 +36,18 @@
 
     //#endregion
 
+    //#region Stringixes
+
+    if (!String.prototype.trim)
+    {
+        String.prototype.trim = function()
+        {
+            return this.replace(/^\s+|\s+$/g, "");
+        };
+    }
+
+    //#endregion
+
     //#region OnNoMatch
 
     // Dictating what should be done when a match is not found - currently used only for DehumanizeTo
@@ -574,6 +586,297 @@
 
         return this.dasherize();
     }
+
+    //#endregion
+
+    //#region NumberToWordsExtension
+
+    function toEnglishWords(number)
+    {
+        if (number === 0)
+        {
+            return "zero";
+        }
+
+        if (number < 0)
+        {
+            return "minus " + Math.abs(number).toWords();
+        }
+
+        var parts = [];
+
+        if ((number / 1000000000) > 0)
+        {
+            parts.push(Math.floor(number / 1000000000).toWords() + " billion");
+            number %= 1000000000;
+        }
+
+        if ((number / 1000000) > 0)
+        {
+            parts.push(Math.floor(number / 1000000).toWords() + " million");
+            number %= 1000000;
+        }
+
+        if ((number / 1000) > 0)
+        {
+            parts.push(Math.floor(number / 1000).toWords() + " thousand");
+            number %= 1000
+        }
+
+        if ((number / 100) > 0)
+        {
+            parts.push(Math.floor(number / 100).toWords() + " hundred");
+            number %= 100;
+        }
+
+        if (number > 0)
+        {
+            if (parts.length !==0)
+            {
+                parts.push("and");
+            }
+
+            var unitsMap = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+            var tensMap = ["zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+            if (number < 20)
+            {
+                parts.push(unitsMap[number]);
+            }
+            else
+            {
+                var lastPart = tensMap[Math.floor(number / 10)];
+                if ((number % 10) > 0)
+                {
+                    lastPart += "-" + unitsMap[number % 10];
+                }
+                parts.push(lastPart);
+            }
+        }
+
+        return parts.join(" ");
+    }
+
+    function toArabicWords(number)
+    {
+        var arabicGroup = [ "مئة", "ألف", "مليون", "مليار", "تريليون", "كوادريليون", "كوينتليون", "سكستيليون" ];
+        var arabicAppendedGroup = [ "", "ألفاً", "مليوناً", "ملياراً", "تريليوناً", "كوادريليوناً", "كوينتليوناً", "سكستيليوناً" ];
+        var arabicPluralGroups = [ "", "آلاف", "ملايين", "مليارات", "تريليونات", "كوادريليونات", "كوينتليونات", "سكستيليونات" ];
+        var onesGroup = [ "", "واحد", "اثنان", "ثلاثة", "أربعة", "خمسة", "ستة", "سبعة", "ثمانية", "تسعة", "عشرة", "أحد عشر", "اثنا عشر", "ثلاثة عشر", "أربعة عشر", "خمسة عشر", "ستة عشر", "سبعة عشر", "ثمانية عشر", "تسعة عشر" ];
+        var tensGroup = [ "", "عشرة", "عشرون", "ثلاثون", "أربعون", "خمسون", "ستون", "سبعون", "ثمانون", "تسعون" ];
+        var hundredsGroup = [ "", "مئة", "مئتان", "ثلاث مئة", "أربع مئة", "خمس مئة", "ست مئة", "سبع مئة", "ثمان مئة", "تسع مئة" ];
+        var arabicAppendedTwos = [ "مئتان", "ألفان", "مليونان", "ملياران", "تريليونان", "كوادريليونان", "كوينتليونان", "سكستيليونلن" ];
+        var arabicTwos = [ "مئتان", "ألفان", "مليونان", "ملياران", "تريليونان", "كوادريليونان", "كوينتليونان", "سكستيليونان" ];
+
+        if (number === 0)
+        {    
+            return "صفر";
+        }
+
+        var result = "";
+        var groupLevel = 0;
+
+        while (number >= 1)
+        {
+            var groupNumber = number % 1000;
+            number = Math.floor(number / 1000);
+
+            var tens = groupNumber % 100;
+            var hundreds = Math.floor(groupNumber / 100);
+            var process = "";
+
+            if (hundreds > 0)
+            {
+                if ((tens === 0) && (hundreds === 2))
+                {
+                    process = arabicAppendedTwos[0];
+                }
+                else
+                {
+                    process = hundredsGroup[hundreds];
+                }
+            }
+
+            if (tens > 0)
+            {
+                if (tens < 20)
+                {
+                    if ((tens == 2) && (hundreds == 0) && (groupLevel > 0))
+                    {
+                        if ((number === 2000) || (number === 2000000) || (number === 2000000000))
+                        {
+                            process = arabicAppendedTwos[groupLevel];
+                        }
+                        else
+                        {
+                            process = arabicTwos[groupLevel];
+                        }
+                    }
+                    else
+                    {
+                        if (process !== "")
+                        {
+                            process += " و ";
+                        }
+
+                        if ((tens === 1) && (groupLevel > 0) && (hundreds === 0))
+                        {
+                            process += " ";
+                        }
+                        else
+                        {
+                            process += onesGroup[tens];
+                        }
+                    }
+                }
+                else
+                {
+                    var ones = tens % 10;
+                    tens = Math.floor(tens / 10);
+
+                    if (ones > 0)
+                    {
+                        if (process !== "")
+                        {
+                            process += " و ";
+                        }
+
+                        process += onesGroup[ones];
+                    }
+
+                    if (process !== "")
+                    {
+                        process += " و ";
+                    }
+
+                    process += tensGroup[tens];
+                }
+            }
+
+            if (process !== "")
+            {
+                if (groupLevel > 0)
+                {
+                    if (result !== "")
+                    {
+                        result = "و " + result;
+                    }
+
+                    if (groupNumber != 2)
+                    {
+                        if (groupNumber % 100 !== 1)
+                        {
+                            if (groupNumber >= 3)
+                            {
+                                result = arabicPluralGroups[groupLevel] + " " + result;
+                            }
+                            else
+                            {
+                                result = (result !== "" ? arabicAppendedGroup[groupLevel] : arabicGroup[groupLevel]) + " " + result;
+                            }
+                        }
+                        else
+                        {
+                            result = arabicGroup[groupLevel] + " " + result;
+                        }
+                    }
+                }
+
+                result = process + " " + result;
+            }
+
+            groupLevel++;
+        }
+
+        return result.trim();
+    }
+
+    Number.prototype.toWords = function ()
+    {
+        /// <summary>
+        ///     3501.ToWords() -> "three thousand five hundred and one"
+        /// </summary>
+
+        if (navigator.language.indexOf("ar") === 0)
+        {
+            return toArabicWords(this);
+        }
+        return toEnglishWords(this);
+    };
+
+    //#endregion
+
+    //#region NumberToOrdinalWordsExtension
+
+    function exceptionNumberToWords (number)
+    {
+        var exceptions = {
+            1: "first",
+            2: "second",
+            3: "third",
+            4: "fourth",
+            5: "fifth",
+            8: "eighth",
+            9: "ninth",
+            12: "twelfth"
+        };
+        if (exceptions.hasOwnProperty(number))
+        {
+            return exceptions[number];
+        }
+        return null;
+    }
+
+    function removeOnePrefix(towords)
+    {
+        if (towords.indexOf("one") === 0)
+        {
+            towords = towords.substr(4);
+        }
+        return towords;
+    }
+
+    function normalNumberToWords(number)
+    {
+        var towords = number.toWords().replace("-", " ");
+
+        towords = removeOnePrefix(towords);
+
+        if (towords.lastIndexOf("y") === towords.length - 1)
+        {
+            towords = towords.substr(0, towords.length - 1) + "ie";
+        }
+
+        return towords + "th";
+    }
+
+    Number.prototype.toOrdinalWords = function ()
+    {
+        /// <summary>
+        ///     1.ToOrdinalWords() -> "first"
+        /// </summary>
+
+        var towords;
+
+        var ex = exceptionNumberToWords(Number);
+        if (ex !== null)
+        {
+            return ex;
+        }
+
+        if (this > 20)
+        {
+            var exceptionPart = exceptionNumberToWords(Number % 10);
+            if (exceptionPart !== null)
+            {
+                var normalPart = this - this % 10;
+                towords = removeOnePrefix(normalPart.toWords());
+                return towords + " " + exceptionPart;
+            }
+        }
+
+        return normalNumberToWords(this);
+    };
 
     //#endregion
 }());
