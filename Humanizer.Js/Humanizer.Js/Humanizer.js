caf551f43b50eeaf6e68f64c79621ec387afa906
Shmueli Englard <shmueli.yosef@englard.net>
2014-06-22 14:05:03 -0400

    Added class NumberToTimeSpanExtensions

--- Humanizer.Js/Humanizer.Js/Humanizer.js
+++ Humanizer.Js/Humanizer.Js/Humanizer.js
@@ -879,4 +879,43 @@
     };
 
     //#endregion
+
+    //#region NumberToTimeSpanExtensions
+
+    var MILLIS_PER_SECOND = 1000;
+    var MILLIS_PER_MINUTE = MILLIS_PER_SECOND * 60;
+    var MILLIS_PER_HOUR = MILLIS_PER_MINUTE * 60;
+    var MILLIS_PER_DAY = MILLIS_PER_HOUR * 24;
+
+    Number.prototype.days = function ()
+    {
+        return this * MILLIS_PER_DAY;
+    };
+
+    Number.prototype.weeks = function ()
+    {
+        return (this * 7).days();
+    };
+
+    Number.prototype.hours = function ()
+    {
+        return this * MILLIS_PER_HOUR;
+    };
+
+    Number.prototype.minutes = function ()
+    {
+        return this * MILLIS_PER_MINUTE;
+    };
+
+    Number.prototype.seconds = function ()
+    {
+        return this * MILLIS_PER_SECOND;
+    };
+
+    Number.prototype.milliseconds = function ()
+    {
+        return this;
+    };
+
+    //#endregion
 }());
