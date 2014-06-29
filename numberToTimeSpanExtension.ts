39ab2bcdca3735758ad5489f070ecb1fcbaebbe9
Shmueli Englard <shmueli.yosef@englard.net>
2014-06-29 16:17:27 -0400

    Added TypeScript fixes

--- numberToTimeSpanExtension.ts
+++ numberToTimeSpanExtension.ts
@@ -1,36 +1,46 @@
-﻿module Humanizer
+﻿interface Number
+{
+    days: () => number;
+    weeks: () => number;
+    hours: () => number;
+    minutes: () => number;
+    seconds: () => number;
+    milliseconds: () => number;
+}
+
+module Humanizer
 {
     var MILLIS_PER_SECOND: number = 1000;
     var MILLIS_PER_MINUTE: number = MILLIS_PER_SECOND * 60;
     var MILLIS_PER_HOUR: number = MILLIS_PER_MINUTE * 60;
     var MILLIS_PER_DAY: number = MILLIS_PER_HOUR * 24;
 
-    Number.prototype.days = function ()
+    Number.prototype.days = function (): number
     {
         return this * MILLIS_PER_DAY;
     };
 
-    Number.prototype.weeks = function ()
+    Number.prototype.weeks = function (): number
     {
         return (this * 7).days();
     };
 
-    Number.prototype.hours = function ()
+    Number.prototype.hours = function (): number
     {
         return this * MILLIS_PER_HOUR;
     };
 
-    Number.prototype.minutes = function ()
+    Number.prototype.minutes = function (): number
     {
         return this * MILLIS_PER_MINUTE;
     };
 
-    Number.prototype.seconds = function ()
+    Number.prototype.seconds = function (): number
     {
         return this * MILLIS_PER_SECOND;
     };
 
-    Number.prototype.milliseconds = function ()
+    Number.prototype.milliseconds = function (): number
     {
         return this;
     };
