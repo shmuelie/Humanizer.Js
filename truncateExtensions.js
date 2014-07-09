aee3520be473991a409354e78d16576dfd12c006
Shmueli Englard <shmueli.yosef@englard.net>
2014-07-08 22:18:47 -0400

    Added truncation extensions

---
+++ truncateExtensions.js
@@ -0,0 +1,29 @@
+﻿var Humanizer;
+(function (Humanizer) {
+    /**
+    * Truncation location for humanizer
+    * @enum
+    * @readonly
+    */
+    (function (TruncateFrom) {
+        /** Truncate letters from the left (start) of the string */
+        TruncateFrom[TruncateFrom["Left"] = 0] = "Left";
+
+        /** Truncate letters from the right (end) of the string */
+        TruncateFrom[TruncateFrom["Right"] = 1] = "Right";
+    })(Humanizer.TruncateFrom || (Humanizer.TruncateFrom = {}));
+    var TruncateFrom = Humanizer.TruncateFrom;
+
+    String.prototype.truncate = function (length, truncator, from) {
+        if (typeof truncator === "undefined") { truncator = Humanizer.Truncator.FixedLength; }
+        if (typeof from === "undefined") { from = 1 /* Right */; }
+        return truncator.truncate(this, length, "…", from);
+    };
+
+    String.prototype.truncateWith = function (length, truncationString, truncator, from) {
+        if (typeof truncator === "undefined") { truncator = Humanizer.Truncator.FixedLength; }
+        if (typeof from === "undefined") { from = 1 /* Right */; }
+        return truncator.truncate(this, length, truncationString, from);
+    };
+})(Humanizer || (Humanizer = {}));
+//# sourceMappingURL=truncateExtensions.js.map
--- truncateExtensions.ts
+++ truncateExtensions.ts
@@ -1,4 +1,10 @@
-﻿module Humanizer
+﻿interface String
+{
+    truncate(length: number, truncator?: Humanizer.ITruncator, from?: Humanizer.TruncateFrom);
+    truncateWith(length: number, truncationString: string, truncator?: Humanizer.ITruncator, from?: Humanizer.TruncateFrom);
+}
+
+module Humanizer
 {
     /**
      * Truncation location for humanizer
@@ -12,4 +18,14 @@
         /** Truncate letters from the right (end) of the string */
         Right
     }
+
+    String.prototype.truncate = function (length: number, truncator: ITruncator = Truncator.FixedLength, from: TruncateFrom = TruncateFrom.Right): string
+    {
+        return truncator.truncate(this, length, "…", from);
+    };
+
+    String.prototype.truncateWith = function (length: number, truncationString: string, truncator: ITruncator = Truncator.FixedLength, from: TruncateFrom = TruncateFrom.Right): string
+    {
+        return truncator.truncate(this, length, truncationString, from);
+    };
 } 
