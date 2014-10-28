338d63b71c546b7839d3354f7ec51b66877a726e
Shmueli Englard <shmueli.yosef@englard.net>
2014-10-28 15:55:07 -0400

    added ByteRate class

---
+++ Bytes/ByteRate.js
@@ -0,0 +1,37 @@
+﻿var Humanizer;
+(function (Humanizer) {
+    (function (Bytes) {
+        "use strict";
+
+        var ByteRate = (function () {
+            function ByteRate(size, interval) {
+                this.size = size;
+                this.interval = interval;
+            }
+            ByteRate.prototype.humanize = function (timeUnit) {
+                if (typeof timeUnit === "undefined") { timeUnit = 1 /* Second */; }
+                var displayInterval;
+                var displayUnit;
+
+                if (timeUnit === 1 /* Second */) {
+                    displayInterval = Number(1).seconds();
+                    displayUnit = "s";
+                } else if (timeUnit === 2 /* Minute */) {
+                    displayInterval = Number(1).minutes();
+                    displayUnit = "min";
+                } else if (timeUnit === 3 /* Hour */) {
+                    displayInterval = Number(1).hours();
+                    displayUnit = "hour";
+                } else {
+                    throw Error("timeUnit must be Second, Minute, or Hour");
+                }
+
+                return (new Bytes.ByteSize(this.size.bytes / this.interval.seconds() * displayInterval.seconds())).toString() + "/" + displayUnit;
+            };
+            return ByteRate;
+        })();
+        Bytes.ByteRate = ByteRate;
+    })(Humanizer.Bytes || (Humanizer.Bytes = {}));
+    var Bytes = Humanizer.Bytes;
+})(Humanizer || (Humanizer = {}));
+//# sourceMappingURL=ByteRate.js.map
---
+++ Bytes/ByteRate.ts
@@ -0,0 +1,44 @@
+﻿module Humanizer.Bytes
+{
+    "use strict";
+
+    export class ByteRate
+    {
+        size: ByteSize;
+        interval: number;
+
+        constructor(size: ByteSize, interval: number)
+        {
+            this.size = size;
+            this.interval = interval;
+        }
+
+        humanize(timeUnit: Humanizer.Localisation.TimeUnit = Humanizer.Localisation.TimeUnit.Second): string
+        {
+            var displayInterval: number;
+            var displayUnit: string;
+
+            if (timeUnit === Humanizer.Localisation.TimeUnit.Second)
+            {
+                displayInterval = Number(1).seconds();
+                displayUnit = "s";
+            }
+            else if (timeUnit === Humanizer.Localisation.TimeUnit.Minute)
+            {
+                displayInterval = Number(1).minutes();
+                displayUnit = "min";
+            }
+            else if (timeUnit === Humanizer.Localisation.TimeUnit.Hour)
+            {
+                displayInterval = Number(1).hours();
+                displayUnit = "hour";
+            }
+            else
+            {
+                throw Error("timeUnit must be Second, Minute, or Hour");
+            }
+
+            return (new ByteSize(this.size.bytes / this.interval.seconds() * displayInterval.seconds())).toString() + "/" + displayUnit;
+        }
+    }
+} 
