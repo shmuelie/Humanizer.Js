f9abd6b40cfe5d7e70a5e90a98bb170bf09f25f3
Shmueli Englard <shmueli.yosef@englard.net>
2014-10-28 15:38:50 -0400

    added TimeUnit enum

---
+++ Localisation/timeUnit.ts
@@ -0,0 +1,19 @@
+﻿module Humanizer.Localisation
+{
+    /**
+     * Units of time.
+     * @enum
+     * @readonly
+     */
+    export enum TimeUnit
+    {
+        Millisecond,
+        Second,
+        Minute,
+        Hour,
+        Day,
+        Week,
+        Month,
+        Year
+    }
+} 
