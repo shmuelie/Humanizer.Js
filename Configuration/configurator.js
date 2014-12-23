7114fea646b84fd78e1e414810afff2dc45de289
Shmueli Englard <shmueli.yosef@englard.net>
2014-12-23 11:34:45 -0500

    updated ordinalizerExtension to work with the new cross language system

--- Configuration/configurator.js
+++ Configuration/configurator.js
@@ -6,10 +6,15 @@ var Humanizer;
         (function (Configurator) {
             "use strict";
             var formatter = new Configuration.FormatterRegistry();
+            var ordinalizers = new Configuration.OrdinalizerRegistry();
             function getFormatter(culture) {
                 return formatter.resolveForCulture(culture);
             }
             Configurator.getFormatter = getFormatter;
+            function getOrdinalizer() {
+                return ordinalizers.resolveForCulture(Humanizer.Resources.getCurrentCulture());
+            }
+            Configurator.getOrdinalizer = getOrdinalizer;
         })(Configurator = Configuration.Configurator || (Configuration.Configurator = {}));
     })(Configuration = Humanizer.Configuration || (Humanizer.Configuration = {}));
 })(Humanizer || (Humanizer = {}));
--- Configuration/configurator.ts
+++ Configuration/configurator.ts
@@ -3,9 +3,15 @@
     "use strict";
 
     var formatter: LocaliserRegistry<Localisation.Formatter.IFormatter> = new FormatterRegistry();
+    var ordinalizers: LocaliserRegistry<Localisation.Ordinalizers.IOrdinalizer> = new OrdinalizerRegistry();
 
     export function getFormatter(culture: string): Localisation.Formatter.IFormatter
     {
         return formatter.resolveForCulture(culture);
+    }
+
+    export function getOrdinalizer(): Localisation.Ordinalizers.IOrdinalizer
+    {
+        return ordinalizers.resolveForCulture(Resources.getCurrentCulture());
     }
 } 
