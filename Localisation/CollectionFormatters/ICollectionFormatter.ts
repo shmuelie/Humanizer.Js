af3d037594927d9b0d146a2986e795f60375e88c
Shmueli Englard <shmueli.yosef@englard.net>
2014-12-30 17:55:19 -0500

    Added ICollectionFormatter

---
+++ Localisation/CollectionFormatters/ICollectionFormatter.ts
@@ -0,0 +1,25 @@
+﻿module Humanizer.Localisation.CollectionFormatters
+{
+    /**
+     * An interface you should implement to localize Humanize for collections
+     */
+    export interface ICollectionFormatter
+    {
+        /**
+         * Formats the collection for display, calling toString() on each object.
+         */
+        humanizer<T>(collection: T[]): string;
+        /**
+         * Formats the collection for display, calling `objectFormatter` on each object.
+         */
+        humanizer<T>(collection: T[], objectFormatter: (item: T) => string): string;
+        /**
+         * Formats the collection for display, calling toString() on each object and using `separator` before the final item.
+         */
+        humanizer<T>(collection: T[], separator: string): string;
+        /**
+         * Formats the collection for display, calling `objectFormatter` on each object and using `separator` before the final item.
+         */
+        humanizer<T>(collection: T[], objectFormatter: (item: T) => string, separator: string): string;
+    }
+} 
