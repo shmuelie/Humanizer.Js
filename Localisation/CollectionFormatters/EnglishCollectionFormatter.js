1a6a14b4699f2326ee50ca75198f67c09b412e6b
Shmueli Englard <shmueli.yosef@englard.net>
2015-01-06 14:11:04 -0500

    Added EnglishCollectionFormatter

---
+++ Localisation/CollectionFormatters/EnglishCollectionFormatter.js
@@ -0,0 +1,49 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Humanizer;
+(function (Humanizer) {
+    (function (Localisation) {
+        (function (CollectionFormatters) {
+            "use strict";
+
+            var EnglishCollectionFormatter = (function (_super) {
+                __extends(EnglishCollectionFormatter, _super);
+                function EnglishCollectionFormatter() {
+                    _super.call(this);
+                    this.defaultSeparator = "and";
+                }
+                EnglishCollectionFormatter.prototype.humanizer_collection_objectFormatter_separator = function (collection, objectFormatter, separator) {
+                    if (collection === null) {
+                        throw new Error("Collection null");
+                    }
+
+                    var length = collection.length;
+
+                    switch (length) {
+                        case 0:
+                            return "";
+                        case 1:
+                            return objectFormatter(collection[0]);
+                        case 2:
+                            return objectFormatter(collection[1]) + " " + separator + " " + objectFormatter(collection[1]);
+                    }
+
+                    var most = [];
+                    for (var i = 0; i < length - 1; i++) {
+                        most.push(objectFormatter(collection[i]));
+                    }
+                    return most.join(", ") + " " + separator + " " + objectFormatter(collection[length - 1]);
+                };
+                return EnglishCollectionFormatter;
+            })(CollectionFormatters.DefaultCollectionFormatter);
+            CollectionFormatters.EnglishCollectionFormatter = EnglishCollectionFormatter;
+        })(Localisation.CollectionFormatters || (Localisation.CollectionFormatters = {}));
+        var CollectionFormatters = Localisation.CollectionFormatters;
+    })(Humanizer.Localisation || (Humanizer.Localisation = {}));
+    var Localisation = Humanizer.Localisation;
+})(Humanizer || (Humanizer = {}));
+//# sourceMappingURL=EnglishCollectionFormatter.js.map
---
+++ Localisation/CollectionFormatters/EnglishCollectionFormatter.ts
@@ -0,0 +1,40 @@
+﻿module Humanizer.Localisation.CollectionFormatters
+{
+    "use strict";
+
+    export class EnglishCollectionFormatter extends DefaultCollectionFormatter
+    {
+        constructor()
+        {
+            super();
+            this.defaultSeparator = "and";
+        }
+
+        humanizer_collection_objectFormatter_separator<T>(collection: T[], objectFormatter: (item: T) => string, separator: string): string
+        {
+            if (collection === null)
+            {
+                throw new Error("Collection null");
+            }
+
+            var length: number = collection.length;
+
+            switch (length)
+            {
+                case 0:
+                    return "";
+                case 1:
+                    return objectFormatter(collection[0]);
+                case 2:
+                    return objectFormatter(collection[1]) + " " + separator + " " + objectFormatter(collection[1]);
+            }
+
+            var most: string[] = [];
+            for (var i: number = 0; i < length - 1; i++)
+            {
+                most.push(objectFormatter(collection[i]));
+            }
+            return most.join(", ") + " " + separator + " " + objectFormatter(collection[length - 1]);
+        }
+    }
+} 
