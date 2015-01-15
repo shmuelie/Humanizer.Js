68737f0cfd1b43648a772d647cb1bb218a92033f
Shmueli Englard <shmueli.yosef@englard.net>
2015-01-15 17:40:25 -0500

    Changed to use mthod overloading, so matches C# better

--- truncateExtensions.js
+++ truncateExtensions.js
@@ -16,16 +16,39 @@
     })(Humanizer.TruncateFrom || (Humanizer.TruncateFrom = {}));
     var TruncateFrom = Humanizer.TruncateFrom;
 
-    String.prototype.truncate = function (length, truncator, from) {
-        if (typeof truncator === "undefined") { truncator = Humanizer.Truncator.FixedLength; }
-        if (typeof from === "undefined") { from = 1 /* Right */; }
-        return truncator.truncate(this, length, "…", from);
-    };
+    var Trunc = (function () {
+        function Trunc() {
+        }
+        Trunc.prototype.truncate = function () {
+            var length = arguments[0];
+            var truncationString = "…";
+            var from = 1 /* Right */;
+            var truncator = Humanizer.Truncator.FixedLength;
+            if (arguments.length > 1) {
+                if (typeof arguments[1] === "string") {
+                    truncationString = arguments[1];
+                } else {
+                    truncator = arguments[1];
+                }
+            }
+            if (arguments.length > 2) {
+                if (typeof arguments[2] === "number") {
+                    from = arguments[2];
+                } else {
+                    truncator = arguments[2];
+                }
+            }
+            if (arguments.length > 3) {
+                from = arguments[3];
+            }
 
-    String.prototype.truncateWith = function (length, truncationString, truncator, from) {
-        if (typeof truncator === "undefined") { truncator = Humanizer.Truncator.FixedLength; }
-        if (typeof from === "undefined") { from = 1 /* Right */; }
-        return truncator.truncate(this, length, truncationString, from);
-    };
+            var $this = this;
+
+            return truncator.truncate($this, length, truncationString, from);
+        };
+        return Trunc;
+    })();
+
+    String.prototype.truncate = Trunc.prototype.truncate;
 })(Humanizer || (Humanizer = {}));
 //# sourceMappingURL=truncateExtensions.js.map
--- truncateExtensions.ts
+++ truncateExtensions.ts
@@ -1,7 +1,9 @@
 ﻿interface String
 {
-    truncate(length: number, truncator?: Humanizer.ITruncator, from?: Humanizer.TruncateFrom): string;
-    truncateWith(length: number, truncationString: string, truncator?: Humanizer.ITruncator, from?: Humanizer.TruncateFrom): string;
+    truncate(length: number): string;
+    truncate(length: number, truncator: Humanizer.ITruncator, from?: Humanizer.TruncateFrom): string;
+    truncate(length: number, truncationString: string, from?: Humanizer.TruncateFrom): string;
+    truncate(length: number, truncationString: string, truncator: Humanizer.ITruncator, from?: Humanizer.TruncateFrom): string;
 }
 
 module Humanizer
@@ -21,13 +23,50 @@ module Humanizer
         Right
     }
 
-    String.prototype.truncate = function (length: number, truncator: ITruncator = Truncator.FixedLength, from: TruncateFrom = TruncateFrom.Right): string
+    class Trunc
     {
-        return truncator.truncate(this, length, "…", from);
-    };
+        truncate(length: number, truncationString: string, truncator: Humanizer.ITruncator, from?: Humanizer.TruncateFrom): string
+        truncate(length: number, truncationString: string, from?: Humanizer.TruncateFrom): string
+        truncate(length: number, truncator: Humanizer.ITruncator, from?: Humanizer.TruncateFrom): string
+        truncate(length: number): string
+        truncate(): string
+        {
+            var length: number = arguments[0];
+            var truncationString: string = "…";
+            var from: TruncateFrom = TruncateFrom.Right;
+            var truncator: ITruncator = Truncator.FixedLength;
+            if (arguments.length > 1)
+            {
+                if (typeof arguments[1] === "string")
+                {
+                    truncationString = arguments[1];
+                }
+                else
+                {
+                    truncator = arguments[1];
+                }
+            }
+            if (arguments.length > 2)
+            {
+                if (typeof arguments[2] === "number")
+                {
+                    from = arguments[2];
+                }
+                else
+                {
+                    truncator = arguments[2];
+                }
+            }
+            if (arguments.length > 3)
+            {
+                from = arguments[3];
+            }
 
-    String.prototype.truncateWith = function (length: number, truncationString: string, truncator: ITruncator = Truncator.FixedLength, from: TruncateFrom = TruncateFrom.Right): string
-    {
-        return truncator.truncate(this, length, truncationString, from);
-    };
+            var $this: string = <string>this;
+
+            return truncator.truncate($this, length, truncationString, from);
+        }
+    }
+
+    String.prototype.truncate = Trunc.prototype.truncate;
 } 
