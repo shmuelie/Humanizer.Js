1340870244f97d04cce09fa60dacd9254fb870e2
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-02 16:15:14 -0500

    Simplified to not use class for overload

--- collectionHumanizeExtensions.js
+++ collectionHumanizeExtensions.js
@@ -2,29 +2,22 @@
 (function (Humanizer) {
     "use strict";
 
-    var Arr = (function () {
-        function Arr() {
+    Array.prototype.humanize = function () {
+        switch (arguments.length) {
+            case 0:
+                return Humanizer.Configuration.Configurator.getCollectionFormatters().humanize(this);
+            case 1:
+                if (typeof arguments[0] === "string") {
+                    return Humanizer.Configuration.Configurator.getCollectionFormatters().humanize(this, arguments[0]);
+                } else {
+                    var df = arguments[0];
+                    return Humanizer.Configuration.Configurator.getCollectionFormatters().humanize(this, df);
+                }
+            case 2:
+                return Humanizer.Configuration.Configurator.getCollectionFormatters().humanize(this, arguments[0], arguments[1]);
+            default:
+                throw new Error("Unknown call");
         }
-        Arr.prototype.humanize = function () {
-            switch (arguments.length) {
-                case 0:
-                    return Humanizer.Configuration.Configurator.getCollectionFormatters().humanize(this);
-                case 1:
-                    if (typeof arguments[0] === "string") {
-                        return Humanizer.Configuration.Configurator.getCollectionFormatters().humanize(this, arguments[0]);
-                    } else {
-                        var df = arguments[0];
-                        return Humanizer.Configuration.Configurator.getCollectionFormatters().humanize(this, df);
-                    }
-                case 2:
-                    return Humanizer.Configuration.Configurator.getCollectionFormatters().humanize(this, arguments[0], arguments[1]);
-                default:
-                    throw new Error("Unknown call");
-            }
-        };
-        return Arr;
-    })();
-
-    Array.prototype.humanize = Arr.prototype.humanize;
+    };
 })(Humanizer || (Humanizer = {}));
 //# sourceMappingURL=collectionHumanizeExtensions.js.map
--- collectionHumanizeExtensions.ts
+++ collectionHumanizeExtensions.ts
@@ -22,47 +22,26 @@ module Humanizer
 {
     "use strict";
 
-    class Arr<T>
+    Array.prototype.humanize = function (): string
     {
-        /**
-         * Formats the collection for display, calling `objectFormatter` on each object and using the default separator for the current culture.
-         */
-        humanize(displayFormatter: (item: T) => string): string;
-        /**
-         * Formats the collection for display, calling ToString() on each object and using the provided separator.
-         */
-        humanize(separator: string): string;
-        /**
-         * Formats the collection for display, calling `objectFormatter` on each object and using the provided separator.
-         */
-        humanize(displayFormatter: (item: T) => string, separator: string): string;
-        /**
-         * Formats the collection for display, calling ToString() on each object and using the default separator for the current culture.
-         */
-        humanize(): string;
-        humanize(): string
+        switch (arguments.length)
         {
-            switch (arguments.length)
-            {
-                case 0:
-                    return Configuration.Configurator.getCollectionFormatters().humanize(<T[]>this);
-                case 1:
-                    if (typeof arguments[0] === "string")
-                    {
-                        return Configuration.Configurator.getCollectionFormatters().humanize(<T[]>this, <string>arguments[0]);
-                    }
-                    else
-                    {
-                        var df: (item: T) => string = arguments[0];
-                        return Configuration.Configurator.getCollectionFormatters().humanize(<T[]>this, df);
-                    }
-                case 2:
-                    return Configuration.Configurator.getCollectionFormatters().humanize(<T[]>this, arguments[0], arguments[1]);
-                default:
-                    throw new Error("Unknown call");
-            }
+            case 0:
+                return Configuration.Configurator.getCollectionFormatters().humanize(this);
+            case 1:
+                if (typeof arguments[0] === "string")
+                {
+                    return Configuration.Configurator.getCollectionFormatters().humanize(this, <string>arguments[0]);
+                }
+                else
+                {
+                    var df: (item: any) => string = arguments[0];
+                    return Configuration.Configurator.getCollectionFormatters().humanize(this, df);
+                }
+            case 2:
+                return Configuration.Configurator.getCollectionFormatters().humanize(this, arguments[0], arguments[1]);
+            default:
+                throw new Error("Unknown call");
         }
-    }
-
-    Array.prototype.humanize = Arr.prototype.humanize;
+    };
 }
